c302a12b5990d9e43acb49d2b3f0a01ea747a2ae
rafaelmanasyan <workpageraf@gmail.com>
2024-10-02 19:51:10 +0300

    'write code for api_class_module.py and vacancy_class.py'

---
+++ project_folder/main.py
@@ -0,0 +1,39 @@
+from project_folder.src.vacancy_class import Vacancy
+from src.api_class_module import HeadHunterAPI
+
+hh_api = HeadHunterAPI()  # Создание экземпляра класса для работы с API сайтов с вакансиями
+hh_vacancies = hh_api.get_vacancies("Python")  # Получение вакансий с hh.ru в формате JSON
+vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)  # Преобразование набора данных из JSON в список объектов
+# Пример работы конструктора класса с одной вакансией
+vacancy = Vacancy("Python Developer",
+                  "<https://hh.ru/vacancy/123456>",
+                  "100 000",
+                  "150 000 руб.",
+                  "Требования: опыт работы от 3 лет...",
+                  '000000000')
+
+# Сохранение информации о вакансиях в файл
+json_saver = JSONSaver()
+json_saver.add_vacancy(vacancy)
+json_saver.delete_vacancy(vacancy)
+
+
+# Функция для взаимодействия с пользователем
+def user_interaction():
+    search_query = input("Введите поисковый запрос: ")
+    vacancies_json_list = HeadHunterAPI().get_vacancies(search_query)
+    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
+    salary_range = input("Введите диапазон зарплат: ") # Пример: 100000 - 150000
+
+    filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
+
+    ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_range)
+
+    sorted_vacancies = sort_vacancies(ranged_vacancies)
+    top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
+    print(top_vacancies)
+
+
+if __name__ == "__main__":
+    user_interaction()
---
+++ project_folder/src/api_class_module.py
@@ -0,0 +1,39 @@
+import requests
+from abc import ABC, abstractmethod
+
+
+class ApiHH(ABC):
+
+    @abstractmethod
+    def get_vacancies(self, keyword):
+        pass
+
+
+class HeadHunterAPI(ApiHH):
+    """
+    Класс для получения данных по вакансиям из API HeadHunter
+    """
+
+    def __init__(self):
+        self.__url = 'https://api.hh.ru/vacancies'
+        self.__headers = {'User-Agent': 'HH-User-Agent'}
+        self.__params = {'text': '', 'page': 0, 'per_page': 100}
+        self.__vacancies = []
+
+    def __get_vacancies(self, keyword):
+        self.__params['text'] = keyword
+        while self.__params.get('page') != 20:
+            response = requests.get(self.__url, headers=self.__headers, params=self.__params)
+            vacancies = response.json()['items']
+            self.__vacancies.extend(vacancies)
+            self.__params['page'] += 1
+
+    def get_vacancies(self, keyword):
+        """
+        Получаем список вакансий в формате json из одноименного приватного метода
+        """
+        self.__get_vacancies(keyword)
+        return self.__vacancies
+
+
+print(HeadHunterAPI().get_vacancies('python'))
